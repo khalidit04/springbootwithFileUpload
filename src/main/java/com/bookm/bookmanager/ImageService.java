126d7af12d637f6f75f20d4e18ba4021c9f4fab8
khalid <khalidit04@gmail.com>
2018-03-11 07:25:44 +0530

    first commit

--- src/main/java/com/bookm/bookmanager/ImageService.java
+++ src/main/java/com/bookm/bookmanager/ImageService.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import org.springframework.data.domain.Pageable;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -64,5 +66,9 @@ public class ImageService {
 
         };
 
+    }
+
+    public Page<Image> findPage(Pageable pageable) {
+        return imageRepository.findAll(pageable);
     }
 }
--- src/main/java/com/bookm/bookmanager/UploadController.java
+++ src/main/java/com/bookm/bookmanager/UploadController.java
@@ -3,15 +3,20 @@ package com.bookm.bookmanager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Pageable;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Controller
 public class UploadController {
@@ -19,7 +24,7 @@ public class UploadController {
     private static final String BASE_PATH="/images";
 
     @Autowired
-    private static  ImageService imageService;
+    private ImageService imageService;
 
     private static final String FILE_NAME="{filename:.+}";
 
@@ -29,6 +34,42 @@ public class UploadController {
 
             Resource file=imageService.findOneImage(filename);
             return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).contentLength(file.contentLength()).body(new InputStreamResource(file.getInputStream()));
+
+    }
+
+    @RequestMapping(value = "/")
+    public String welcome(Model model, Pageable pageable){
+            final Page<Image> page=imageService.findPage(pageable);
+            model.addAttribute("page",page);
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = BASE_PATH)
+    @ResponseBody
+    public ResponseEntity<?> createImageFile(@RequestParam("file")MultipartFile file, HttpServletRequest request){
+
+        try {
+            imageService.createImage(file);
+            final URI locationURI=new URI(request.getRequestURL().toString()+"/").resolve(file.getOriginalFilename()+"/raw");
+
+            return ResponseEntity.created(locationURI).body("successfully upload"+file.getOriginalFilename());
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload"+file.getOriginalFilename()+"=>"+e.getMessage());
+        }
+    }
+
+
+    @RequestMapping(method = RequestMethod.DELETE,value=BASE_PATH+"/"+FILE_NAME)
+    @ResponseBody
+    public ResponseEntity<?> deleteImage(@PathVariable String filename){
+        try {
+            imageService.deleteImage(filename);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Successfully deleted "+filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete "+filename);
+        }
 
     }
 }
